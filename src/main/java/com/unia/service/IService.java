28fc02b02bb28760bce9e132af871127f0a77fc6
i7 <i7@VPITI.unia.edu.pe>
2018-11-22 15:49:11 -0500

    nuevo grafico de barras

--- src/main/java/com/unia/service/IService.java
+++ src/main/java/com/unia/service/IService.java
@@ -2,10 +2,10 @@ package com.unia.service;
 
 import java.util.List;
 
-public interface IService <T> {
-	void registrar (T t)throws Exception;
-	void modificar(T t)throws Exception;
-	List<T> listar()throws Exception;
-	T listarPorId(T t)throws Exception;
-
+public interface IService<T> {
+	
+	void registrar (T t) throws Exception;
+	void modificar (T t) throws Exception;
+	List<T> listar () throws Exception;
+	T listarPorId (T t) throws Exception;
 }
